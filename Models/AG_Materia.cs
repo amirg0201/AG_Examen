3020258e5d46b9cd12c175bfdc5aac3cbb8aa21d
(Estudiante) Amir Alejandro García Villamarín <amir.garcia@udla.edu.ec>
2024-04-24 10:53:56 -0500

    Agregar archivos de proyecto.

---
+++ Models/AG_Materia.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AG_Materias.Models
+{
+    public class AG_Materia
+    {
+        int Id { get; set; }
+        public string materia { get; set; }
+        public string name { get; set; }
+        public bool aprobado { get; set; }
+        [VerificarRango]
+        public decimal nota { get; set; }
+    }
+
+
+    public class VerificarRango : ValidationAttribute
+    {
+        public override bool IsValid(object? value)
+        {
+            decimal valor = (decimal)value;
+            if (valor < 10)
+            {
+                return base.IsValid(value);
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
+
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AG_Materias.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
